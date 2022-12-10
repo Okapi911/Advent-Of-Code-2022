669fa494fc08624294c13909909fce2039693d8f
Okapi911 <lucas.kloubert@gmail.com>
2022-12-10 16:13:31 +0100

    day10 V2

--- 2022/day10/day10.ts
+++ 2022/day10/day10.ts
@@ -2,14 +2,14 @@ import * as fs from 'fs';
 
 const tabl: string[] = fs.readFileSync('./2022/day10/input10.in', 'utf-8').split('\n');
 
-let X = 1;
-let turn = 0;
-let result1 = 0;
-let result2 = Array.from(Array(6).keys()).map((value) => Array.from(Array(40).keys()).map((value) => '.'));
+let X: number = 1;
+let turn: number = 0;
+let result1: number = 0;
+let result2: string[][] = Array.from(Array(6).keys()).map((value) => Array.from(Array(40).keys()).map((value) => '.'));
 
 function execCom(memory: number, commandBloc: string, repeat: boolean = false): void {
-    let row = Math.floor(turn / 40);
-    let column = turn % 40;
+    let row: number = Math.floor(turn / 40);
+    let column: number = turn % 40;
     if (Math.abs(column - X) <= 1) {
         result2[row][column] = result2[row][column].replace('.', '#');
     }
@@ -18,7 +18,7 @@ function execCom(memory: number, commandBloc: string, repeat: boolean = false): 
         result1 += X * turn;
     }
     X += memory;
-    let command = commandBloc.split(' ');
+    let command: string[] = commandBloc.split(' ');
     if (command.length != 1 && !repeat) {
         execCom(Number(command[1]), commandBloc, true);
     }
