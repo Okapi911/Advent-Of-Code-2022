2f9951c5a83c27cfe0e48214e292cea8f2700ac9
Okapi911 <lucas.kloubert@gmail.com>
2022-12-14 15:13:59 +0100

    day14 final

--- 2022/day14/day14.ts
+++ 2022/day14/day14.ts
@@ -1,19 +1,19 @@
 import * as fs from 'fs';
 console.time('yeah');
-let tabl = fs
+let tabl: number[][][] = fs
     .readFileSync('./2022/day14/input14.in', 'utf-8')
     .split('\n')
     .map((value) => value.split(' -> ').map((value2) => value2.split(',').map(Number)));
 
-let maxHeight = 0;
-let minWidth = Infinity;
-let maxWidth = 0;
+let maxHeight: number = 0;
+let minWidth: number = Infinity;
+let maxWidth: number = 0;
 
-let solidsPositions = new Set();
+let solidsPositions: Set<string> = new Set();
 
 function solidPositionsIni() {
     tabl.map((barrier) => {
-        let indexOfWall = 0;
+        let indexOfWall: number = 0;
         while (indexOfWall < barrier.length - 1) {
             drawWall(barrier[indexOfWall], barrier[indexOfWall + 1]);
             indexOfWall += 1;
@@ -42,24 +42,24 @@ function drawWall(point1: number[], point2: number[]): void {
         maxHeight = point2[1];
     }
     if (point1[0] == point2[0]) {
-        let direction = (point1[1] - point2[1]) / Math.abs(point1[1] - point2[1]);
+        let direction: number = (point1[1] - point2[1]) / Math.abs(point1[1] - point2[1]);
         Array.from(Array(Math.abs(point1[1] - point2[1]) + 1).keys()).map((value) => {
             solidsPositions.add([point1[0], point1[1] - direction * value].join('-'));
         });
     }
     if (point1[1] == point2[1]) {
-        let direction = (point1[0] - point2[0]) / Math.abs(point1[0] - point2[0]);
+        let direction: number = (point1[0] - point2[0]) / Math.abs(point1[0] - point2[0]);
         Array.from(Array(Math.abs(point1[0] - point2[0]) + 1).keys()).map((value) => {
             solidsPositions.add([point1[0] - direction * value, point1[1]].join('-'));
         });
     }
 }
 
-let sand = -1;
+let sand: number = -1;
 while (true) {
     sand += 1;
-    let sandPosition = [500, 0];
-    let breaker = moveSand(sandPosition);
+    let sandPosition: number[] = [500, 0];
+    let breaker: boolean = moveSand(sandPosition);
     if (breaker) {
         break;
     }
@@ -90,7 +90,7 @@ solidPositionsIni();
 maxHeight += 2;
 maxWidth = Infinity;
 minWidth = -Infinity;
-for (let index = 0; index < 10000; index++) {
+for (let index: number = 0; index < 10000; index++) {
     solidsPositions.add([index, maxHeight].join('-'));
     solidsPositions.add([-index, maxHeight].join('-'));
 }
@@ -98,8 +98,8 @@ for (let index = 0; index < 10000; index++) {
 sand = 0;
 while (!solidsPositions.has('500-0')) {
     sand += 1;
-    let sandPosition = [500, 0];
-    let breaker = moveSand(sandPosition);
+    let sandPosition: number[] = [500, 0];
+    let breaker: boolean = moveSand(sandPosition);
 }
 console.log(sand);
 console.timeEnd('yeah');
